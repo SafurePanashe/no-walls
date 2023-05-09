c4b7f516d79750d1ef5557827bb3c999aa499b95
SafurePanashe <Safurepanashe17@gmail.com>
2023-05-09 09:48:11 +0200

    add html

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+
+    let ham = document.querySelector(".hamburger");
+    let hd  = document.querySelector("header");
+
+
+  ham.addEventListener('click',function(){
+    hd.classList.toggle('open');
+  })
