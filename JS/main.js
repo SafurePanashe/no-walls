e3f784f407a650b764acb511cd9c3a513de81a1d
SafurePanashe <Safurepanashe17@gmail.com>
2023-05-10 12:24:38 +0200

    added js

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,37 @@
 
-    let ham = document.querySelector(".hamburger");
-    let hd  = document.querySelector("header");
+let ham = document.querySelector(".hamburger");
+let hd = document.querySelector("header");
 
 
-  ham.addEventListener('click',function(){
-    hd.classList.toggle('open');
-  })
+ham.addEventListener('click', function () {
+  hd.classList.toggle('open');
+})
+
+$(document).ready(function () {
+  $('.slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+     
+  });
+});
